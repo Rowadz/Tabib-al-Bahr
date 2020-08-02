9715abdbb2432a7e866077b272205f2208000bd1
MohammedAl-rowad <rowadwo@gmail.com>
2020-08-02 23:45:44 +0300

    better home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import doctor1 from './doctor1.svg'
 import doctor2 from './doctor2.svg'
-import { Grid, Row, Col } from 'rsuite'
+import { Grid, Row, Col, Panel } from 'rsuite'
 
 export default function Home() {
   return (
-    <Grid style={{ padding: 70 }}>
+    <Grid style={{ padding: 70, textAlign: 'center' }}>
       <Row>
-        <Col xs={24} sm={24} md={12} style={{ padding: 70 }}>
-          <img src={doctor1} alt="doctor1" width="100%" height="200" />
+        <Col xs={24} sm={24} md={12}>
+          <Panel bodyFill style={{ display: 'inline-block' }}>
+            <img src={doctor1} alt="doctor1" width="100%" height="auto" />
+            <Panel>
+              <h1>نظام بسيط لإداره العيادات</h1>
+            </Panel>
+          </Panel>
         </Col>
-        <Col xs={24} sm={24} md={12} style={{ padding: 70 }}>
-          <h1>نظام بسيط لإداره العيادات</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={24} sm={24} md={12} style={{ padding: 70 }}>
-          <h1>يمكنك إضافـة معلومات عن الأطباء و المرضى و رؤية إحصائيـات</h1>
-        </Col>
-        <Col xs={24} sm={24} md={12} style={{ padding: 70 }}>
-          <img src={doctor2} alt="doctor2" width="100%" height="200" />
+        <Col xs={24} sm={24} md={12}>
+          <Panel bodyFill style={{ display: 'inline-block' }}>
+            <img src={doctor2} alt="doctor2" width="100%" height="auto" />
+            <Panel>
+              <h1>يمكنك إضافـة معلومات عن الأطباء و المرضى و رؤية إحصائيـات</h1>
+            </Panel>
+          </Panel>
         </Col>
       </Row>
     </Grid>
