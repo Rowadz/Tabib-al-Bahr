4d32d899afc110f91763bd4df40c840f0b7c4d89
MohammedAl-rowad <rowadwo@gmail.com>
2020-08-02 22:58:26 +0300

    arabic :)

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { IntlProvider } from 'rsuite'
+import { IntlProviderProps } from 'rsuite/lib/IntlProvider'
+
+function RTL(props: IntlProviderProps) {
+  return <IntlProvider rtl>{props.children}</IntlProvider>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RTL>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RTL>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
